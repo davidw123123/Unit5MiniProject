9e12e527bd1b7048688768bee49d405bbaf1222f
BT_2S14_19 <davidwg1210@gmail.com>
2024-12-06 10:52:06 -0500

    finsihed test cases

--- src/TImeTester.java
+++ src/TImeTester.java
@@ -1,20 +1,27 @@
 public class TImeTester {
     public static void main(String[] args) {
         Time time = new Time(23,58,58);
+        System.out.println("EXPECTED RESULT: 23:58:58");
+        System.out.println("ACTUAL RESULT: "+time);
+        System.out.println();
         time.tick();
-        System.out.println(time);
+        System.out.println("EXPECTED RESULT: 23:58:59");
+        System.out.println("ACTUAL RESULT: "+time);
+        System.out.println();
         time.tick();
-        System.out.println(time);
+        System.out.println("EXPECTED RESULT: 23:59:00");
+        System.out.println("ACTUAL RESULT: "+time);
+        System.out.println();
         for (int i = 0;i < 60; i++){
            time.tick();
         }
-        System.out.println(time);
-        time.tick();
-        time.tick();
-        System.out.println(time);
+        System.out.println("EXPECTED RESULT: 00:00:00");
+        System.out.println("ACTUAL RESULT: "+time);
+        System.out.println();
         Time time2 = new Time(18,45,12);
         Time time3 = new Time(7,20,50);
         time2.add(time3);
-        System.out.println(time2);
+        System.out.println("EXPECTED RESULT: 02:06:02");
+        System.out.println("ACTUAL RESULT: "+time2);
     }
 }
